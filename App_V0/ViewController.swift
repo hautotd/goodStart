9f51cb0c0e22e047feeea673edd1ba28efb148ab
hdim <hautot.d@gmail.com>
2014-08-23 21:18:21 +0200

    Initial Commit

---
+++ App_V0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_V0
+//
+//  Created by Dimitri Hautot on 23/08/2014.
+//  Copyright (c) 2014 Dimitri Hautot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
