837d83a010d3aa5a4cb380a15212e52eb43ad7ae
hdim <hautot.d@gmail.com>
2014-08-24 19:46:43 +0200

    first commit

---
+++ App_V0/ConnectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConnectionViewController.swift
+//  App_V0
+//
+//  Created by Dimitri Hautot on 23/08/2014.
+//  Copyright (c) 2014 Dimitri Hautot. All rights reserved.
+//
+
+import Foundation
---
+++ App_V0/newConquestViewController.swift
@@ -0,0 +1,9 @@
+//
+//  newConquestViewController.swift
+//  App_V0
+//
+//  Created by Dimitri Hautot on 24/08/2014.
+//  Copyright (c) 2014 Dimitri Hautot. All rights reserved.
+//
+
+import Foundation
